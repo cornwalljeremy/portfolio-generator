bdea95d341a7e9c017a9007517f330de12c4445b
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-09-23 10:57:29 -0600

    eh?

--- app.js
+++ app.js
@@ -1,16 +1,92 @@
 // console.log('Hello Node')
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
-const inquirer = require('inquirer');
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
+const inquirer = require("inquirer");
 
-inquirer.prompt([
+const promptUser = () => {
+  return inquirer.prompt([
     {
-        type: 'input',
-        name: 'name',
-        message: 'What Is Your Name?'
+      type: "input",
+      name: "name",
+      message: "What Is Your Name?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "enter your github user name",
+    },
+    {
+      type: "input",
+      name: "about",
+      message: "Provide info about you",
+    },
+  ]);
+};
+
+
+const promptProject = portfolioData => {
+  console.log(` 
+    =================
+    Add a New Project
+    =================`);
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
     }
-])
-.then(answers => console.log(answers));
+
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the name of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a description of the project (Required)",
+    },
+    {
+      type: "checkbox",
+      name: "languages",
+      message: "What did you build this project with? (Check all that apply)",
+      choices: [
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "ES6",
+        "jQuery",
+        "Bootstrap",
+        "Node",
+      ],
+    },
+    {
+      type: "input",
+      name: "link",
+      message: "Enter the GitHub link to your project. (Required)",
+    },
+    {
+      type: "confirm",
+      name: "feature",
+      message: "Would you like to feature this project?",
+      default: false,
+    },
+    {
+      type: "confirm",
+      name: "confirmAddProject",
+      message: "Would you like to enter another project?",
+      default: false,
+    },
+  ]);
+  
+};
+
+
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+    console.log(portfolioData);
+})
+
+
 
 // console.log(inquirer)
 // const pageHTML = generatePage(name, github);
@@ -21,11 +97,8 @@ inquirer.prompt([
 
 // const [name, github] = profileDataArgs;
 
-
-
-
 // fs.writeFile('./index.html', pageHTML, err => {
 //     if(err) throw new Error(err);
-    
+
 //     console.log('Portfolio complete! Checkout index.html to see the output!');
 // })
