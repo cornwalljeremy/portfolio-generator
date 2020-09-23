f50b531758f64b20569415f8cd2e47efc4206e2c
cornwalljeremy <cornwall.jeremy@gmail.com>
2020-09-23 10:21:43 -0600

    inquirer

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 // console.log('Hello Node')
 const fs = require('fs');
 const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// const name = profileDataArgs[0];
+const inquirer = require('inquirer');
+
+inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What Is Your Name?'
+    }
+])
+.then(answers => console.log(answers));
+
+// console.log(inquirer)
+// const pageHTML = generatePage(name, github);
+
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// // const name = profileDataArgs[0];
 // const github = profileDataArgs[1];
 
-const [name, github] = profileDataArgs;
+// const [name, github] = profileDataArgs;
 
 
 
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if(err) throw new Error(err);
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if(err) throw new Error(err);
     
-    console.log('Portfolio complete! Checkout index.html to see the output!');
-})
+//     console.log('Portfolio complete! Checkout index.html to see the output!');
+// })
